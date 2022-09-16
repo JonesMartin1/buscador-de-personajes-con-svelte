37f003d2ee64785d964935a3d9a201a8967e6d51
Martin <martin.jones.r@gmail.com>
2022-09-15 21:07:39 -0300

    correcciòn

--- myapp/src/App.svelte
+++ myapp/src/App.svelte
@@ -2,8 +2,13 @@
   import Input from './lib/Input.svelte';
 </script>
 
+<h1>Buscador de Personajes de Marvel</h1>
+
 <main>
+  <img class="personajes" alt="imagen" src="https://i.blogs.es/1aad84/marvel/1366_521.jpeg">
+  <h1> </h1>
   <Input>
+
   </Input>
 </main>
 
